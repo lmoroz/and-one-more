2e14198c6d108f06d6dfdaf4c0c9f7a9903a1d36
Larisa Moroz <larisa.moroz@gmail.com>
2017-07-10 01:19:01 +0800

    koa-router middleware experiment

--- ruslotto-checker.js
+++ ruslotto-checker.js
@@ -3,11 +3,12 @@
 const config = require('config');
 const Koa = require('koa');
 const Router = require('koa-router');
-const cors = require('@koa/cors');
+const cors = require('kcors');
 const path = require('path');
 const fs = require('fs');
 const pick = require('lodash/pick');
 const createError = require('http-errors');
+const koaRequest = require('koa-http-request');
 
 const app = new Koa();
 
@@ -17,6 +18,14 @@ handlers.forEach(handler => {
 });
 
 app.use(cors({ allowMethods: 'GET,HEAD,POST' }));
+
+async function setDrawCacheId(ctx, next) {
+  const seekDraw = ctx.params.draw;
+  ctx.params.cacheName = 'ruslotto'+seekDraw+'.html';
+	console.log('ctx.params.cacheName = ',ctx.params.cacheName);
+	await next();
+	ctx.body += ' got from setDrawCacheId()';
+}
 
 const router = new Router({
     prefix: '/api'
@@ -29,21 +38,22 @@ const router = new Router({
           ctx.throw(createError(422, 'Unknown error'));
       }
   })
-  .get('/data/:draw', async ctx => {
+  .get('/data/:draw', setDrawCacheId, async ctx => {
       try {
           const seekDraw = ctx.params.draw;
+          ctx.body = 'Get data for the draw № '+seekDraw;
 
-          if (!('OFFERS' in jjConfig) || !Array.isArray(jjConfig.OFFERS))
-              ctx.throw(createError(422, 'OFFERS array required'));
-
-          const resJson = jjConfig.OFFERS.map((offer) => {
-              const offerShort = pick(offer, ['NAME', 'DISPLAY_PROPERTIES']);
-              offerShort.DETAIL_PICTURE = pick(offer.DETAIL_PICTURE, ['SRC']);
-              offerShort.SLIDER = offer.SLIDER.map(sliderItem => pick(sliderItem, ['SRC']));
-              return offerShort;
-          });
-
-          ctx.body = {OFFERS: resJson};
+//           if (!('OFFERS' in jjConfig) || !Array.isArray(jjConfig.OFFERS))
+//               ctx.throw(createError(422, 'OFFERS array required'));
+// 
+//           const resJson = jjConfig.OFFERS.map((offer) => {
+//               const offerShort = pick(offer, ['NAME', 'DISPLAY_PROPERTIES']);
+//               offerShort.DETAIL_PICTURE = pick(offer.DETAIL_PICTURE, ['SRC']);
+//               offerShort.SLIDER = offer.SLIDER.map(sliderItem => pick(sliderItem, ['SRC']));
+//               return offerShort;
+//           });
+// 
+//           ctx.body = {OFFERS: resJson};
       }
       catch (e) {
           ctx.throw(createError(422, 'Draw number required'));
@@ -51,7 +61,7 @@ const router = new Router({
   })
   .post('/check', async ctx => {
       try {
-          const ticketData = eval(`new Object(${ctx.request.body})`);
+          const ticketData = JSON.parse(ctx.request.body);
 
           if (!('tickets' in ticketData) || !Array.isArray(ticketData.tickets))
               ctx.throw(createError(422, 'Tickets numbers array required'));
