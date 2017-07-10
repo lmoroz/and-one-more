58bae64e54704f893bb84498573812218314ee7b
Larisa Moroz <larisa.moroz@gmail.com>
2017-07-10 22:27:42 +0800

    add axios instead of koa-http-request

--- ruslotto-checker.js
+++ ruslotto-checker.js
@@ -1,83 +1,81 @@
 'use strict';
 
-const config = require('config');
-const Koa = require('koa');
-const Router = require('koa-router');
-const cors = require('kcors');
-const path = require('path');
-const fs = require('fs');
-const pick = require('lodash/pick');
+const config      = require('config');
+const Koa         = require('koa');
+const Router      = require('koa-router');
+const cors        = require('kcors');
+const path        = require('path');
+const fs          = require('fs');
+const pick        = require('lodash/pick');
 const createError = require('http-errors');
-const koaRequest = require('koa-http-request');
+const axios       = require('axios');
 
 const app = new Koa();
 
 const handlers = fs.readdirSync(path.join(__dirname, 'handlers')).sort();
 handlers.forEach(handler => {
-    require(`./handlers/${handler}`).init(app);
+  require(`./handlers/${handler}`).init(app);
 });
 
-app.use(cors({ allowMethods: 'GET,HEAD,POST' }));
+const aDay   = 1000 * 60 * 60 * 24;
+const aWeek  = aDay * 7;
+const aMonth = aDay * 30;
+const aYear  = aDay * 336;
+app.use(cors({allowMethods: 'GET,HEAD,POST', maxAge: aYear}));
 
-async function setDrawCacheId(ctx, next) {
-  const seekDraw = ctx.params.draw;
-  ctx.params.cacheName = 'ruslotto'+seekDraw+'.html';
-  console.log('ctx.params.cacheName = ',ctx.params.cacheName);
+async function setDrawCacheId ( ctx, next ) {
+  const seekDraw       = ctx.params.draw;
+  ctx.params.cacheName = 'ruslotto' + seekDraw + '.html';
+  console.log(`ctx.params.cacheName = ${ctx.params.cacheName}`);
   await next();
-  ctx.body += ' got from setDrawCacheId()';
+  ctx.body += "\r\ngot from setDrawCacheId()";
+}
+
+async function getRuslottoDraw ( ctx, next ) {
+  try {
+    const drawUrl = `https://m.stoloto.ru/ruslotto/archive/${ctx.params.draw}`;
+    console.log(`drawUrl = ${drawUrl}; getRuslottoDraw() drawID = ${ctx.params.draw}`);
+    let drawHTML = await axios.get(drawUrl, {responseType: 'document', timeout: 5000});
+    ctx.body     = drawHTML.data;
+  }
+  catch (e) {
+    console.error(e);
+    ctx.throw(createError(404, `Не удалось скачать данные о тираже № ${ctx.params.draw} Русского лото`));
+  }
 }
 
 const router = new Router({
-    prefix: '/api'
-})
+                            prefix: '/api'
+                          })
   .get('/help', async ctx => {
-      try {
-          ctx.body = 'Get help for the app';
-      }
-      catch (e) {
-          ctx.throw(createError(422, 'Unknown error'));
-      }
+    try {
+      ctx.body = 'Get help for the app';
+    }
+    catch (e) {
+      ctx.throw(createError(422, 'Unknown error'));
+    }
   })
-  .get('/data/:draw', setDrawCacheId, async ctx => {
-      try {
-          const seekDraw = ctx.params.draw;
-          ctx.body = 'Get data for the draw № '+seekDraw;
-
-//           if (!('OFFERS' in jjConfig) || !Array.isArray(jjConfig.OFFERS))
-//               ctx.throw(createError(422, 'OFFERS array required'));
-// 
-//           const resJson = jjConfig.OFFERS.map((offer) => {
-//               const offerShort = pick(offer, ['NAME', 'DISPLAY_PROPERTIES']);
-//               offerShort.DETAIL_PICTURE = pick(offer.DETAIL_PICTURE, ['SRC']);
-//               offerShort.SLIDER = offer.SLIDER.map(sliderItem => pick(sliderItem, ['SRC']));
-//               return offerShort;
-//           });
-// 
-//           ctx.body = {OFFERS: resJson};
-      }
-      catch (e) {
-          ctx.throw(createError(422, 'Draw number required'));
-      }
+  .get('/data/:draw', setDrawCacheId, getRuslottoDraw, async ctx => {
   })
   .post('/check', async ctx => {
-      try {
-          const ticketData = JSON.parse(ctx.request.body);
+    try {
+      const ticketData = JSON.parse(ctx.request.body);
 
-          if (!('tickets' in ticketData) || !Array.isArray(ticketData.tickets))
-              ctx.throw(createError(422, 'Tickets numbers array required'));
+      if (!( 'tickets' in ticketData ) || !Array.isArray(ticketData.tickets))
+        ctx.throw(createError(422, 'Tickets numbers array required'));
 
-          const resJson = tickeData.numbers.map((number) => {
-              const offerShort = pick(offer, ['NAME', 'DISPLAY_PROPERTIES']);
-              offerShort.DETAIL_PICTURE = pick(offer.DETAIL_PICTURE, ['SRC']);
-              offerShort.SLIDER = offer.SLIDER.map(sliderItem => pick(sliderItem, ['SRC']));
-              return offerShort;
-          });
+      const resJson = tickeData.numbers.map(( number ) => {
+        const offerShort          = pick(offer, [ 'NAME', 'DISPLAY_PROPERTIES' ]);
+        offerShort.DETAIL_PICTURE = pick(offer.DETAIL_PICTURE, [ 'SRC' ]);
+        offerShort.SLIDER         = offer.SLIDER.map(sliderItem => pick(sliderItem, [ 'SRC' ]));
+        return offerShort;
+      });
 
-          ctx.body = {result: resJson};
-      }
-      catch (e) {
-          ctx.throw(createError(422, 'Tickets numbers array required'));
-      }
+      ctx.body = {result: resJson};
+    }
+    catch (e) {
+      ctx.throw(createError(422, 'Tickets numbers array required'));
+    }
   });
 
 app.use(router.routes());
